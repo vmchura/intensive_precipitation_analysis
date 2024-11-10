290cdb122333eec32ce69a3fe6b36f363ee7477e
Vicc <vmchura@gmail.com>
2024-11-10 23:12:18 +0100

    001.CompareDataSets.py

---
+++ EDA/001.CompareDataSets.py
@@ -0,0 +1,52 @@
+import pandas as pd
+from dotenv import load_dotenv
+import os
+import netCDF4
+from matplotlib import pyplot as plt
+import numpy as np
+plt.rcParams['figure.figsize'] = [10, 8]
+
+if __name__ == "__main__":
+    load_dotenv()
+    data_path = os.getenv("EL_NINO_DATA_PATH")
+    output_figures_path = os.getenv("FIGURE_OUTPUT")
+
+
+    precipitation_hourly_nc = netCDF4.Dataset(f'{data_path}/PISCOp_h_non-DBC_2015.nc')
+
+    def find_nearest_index(array, value):
+        return np.abs(array - value).argmin()
+
+    latitudes = precipitation_hourly_nc.variables['latitude'][:]
+    longitudes = precipitation_hourly_nc.variables['longitude'][:]
+
+    # Latitude and longitude for La Esperanza station
+    target_latitude = -4.913735
+    target_longitude = -81.055118
+
+    lat_index = find_nearest_index(latitudes, target_latitude)
+    lon_index = find_nearest_index(longitudes, target_longitude)
+    nearly_station_precipitation_hourly = precipitation_hourly_nc.variables['p'][:, lat_index, lon_index]
+    nearly_station_precipitation_daily = np.sum(nearly_station_precipitation_hourly.reshape(-1, 24), axis=1)
+    nearly_station_dates = np.arange('2015-01-01', '2016-01-01', dtype='datetime64[D]')
+    nearly_station_hours = precipitation_hourly_nc.variables['time'][:].data
+    assert np.all(nearly_station_hours[:-1] <= nearly_station_hours[1:]), 'time is not sorted'
+    assert len(nearly_station_precipitation_daily) == len(nearly_station_dates), 'not all days with data'
+
+    nearly_station_df = pd.DataFrame({'date':nearly_station_dates, 'sum_hourly':nearly_station_precipitation_daily})
+
+    station_df = pd.read_csv(f'{data_path}/qc00000230.txt', header=None, delimiter=r"\s+")
+    station_df.columns =['year', 'month', 'day', 'prep', 'temp_min', 'temp_max']
+    station_df = station_df.assign(date=lambda x: pd.to_datetime(x['year'].apply(str) +
+                                                                 x['month'].apply(str).str.zfill(2) +
+                                                                 x['day'].apply(str).str.zfill(2), format='%Y%m%d'))
+
+    station_2015 = station_df[station_df['date'].dt.year == 2015]
+
+    fig, ax = plt.subplots()
+    ax = station_2015.plot(ax=ax, x='date', y='prep', c='red', label='Precipitation registered by Station La Esperanza', marker='o',linestyle='None')
+    _ = nearly_station_df.plot(ax=ax, kind='line', x='date', y='sum_hourly', c='cyan', label='High-resolution gridded hourly precipitation close to Station La Esperanza', linestyle='solid')
+    plt.title("Comparison between two different precipitation datasets for year 2015")
+    fig.savefig(f'{output_figures_path}/cmp_station_nearly_station.png')   # save the figure to file
+    plt.close(fig)
+    print('plot generated')
---
+++ README.md
@@ -0,0 +1,27 @@
+# Prediction of high precipitation on Piura / Perú
+
+Although there are already highly accurate methods for predicting precipitation on the western side of South America, my
+particular interest is in understanding why periods of intensive precipitation have become more frequent in recent years
+compared to past decades.
+
+As a secondary goal, I aim to enhance my skills in techniques used in Data Engineering and Data Science. To that end, I
+will do my best to document every step of this project.
+
+## Content
+
+* EDA
+    * 001.CompareDataSets.py
+        * Script to compare complementary sources, one dataset spans from 1996 to 2015-12, and the other from 2015-01 to 2020-12.
+        * The comparison focuses on overlapping days to adjust the datasets so they align on the same scale.
+
+## Data Sets used
+
+* data (directory not shared in this repository)
+    * PISCOp_h_non-DBC_2015.nc
+        * [Details on the article](https://www.sciencedirect.com/science/article/pii/S2352340922007776?via%3Dihub)
+        * [URL where the dataset is hosted.](https://figshare.com/articles/dataset/SATc/17148416?backTo=/collections/Development_of_high-resolution_hourly_gridded_precipitation_dataset_over_Peru/5743166)
+    * qc00000230.txt
+        * Precipitation data for station "La Esperanza", Perú/Piura/Paita/Colan
+        * URL where the dataset can be downloaded previous
+          registration [Senhami](https://www.senamhi.gob.pe/site/descarga-datos/)
+    
